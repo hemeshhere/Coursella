13536058bdb1e191799e6fd7f061a63f0edec3d2
hemeshhere <hh9199939655@gmail.com>
2025-05-27 15:45:46 +0530

    initialising files

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+app.post("/signup", (req,res)=>{
+
+});
+
+app.post("/login", (req,res)=>{
+
+});
+app.post("/purchaseCourse", (req,res)=>{
+
+});
+app.get("/all Courses", (req,res)=>{
+
+});
+app.get("/myCourses", (req,res)=>{
+
+});
+
+
+app.listen(3000);
